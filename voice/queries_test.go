ecf1d7b1c35d7c68f581120693674521f5ac35c4
James Tan <james_tan97@outlook.com>
2020-03-29 19:41:11 +0800

    Add unit tests for Song queries

---
+++ voice/queries_test.go
@@ -0,0 +1,73 @@
+package voice_test
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jamestjw/lyrical/database"
+	"github.com/jamestjw/lyrical/voice"
+	"github.com/stretchr/testify/assert"
+)
+
+func setup() *sql.DB {
+	testConnection, err := sql.Open("sqlite3", "../db/test.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	database.Connection = testConnection
+
+	statement, err := testConnection.Prepare("CREATE TABLE IF NOT EXISTS songs (id INTEGER PRIMARY KEY, youtube_id TEXT, name TEXT, created_at TIMESTAMP)")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer statement.Close()
+	statement.Exec()
+
+	return testConnection
+}
+
+func teardown(c *sql.DB) {
+	statement, err := c.Prepare("DROP TABLE IF EXISTS songs")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer statement.Close()
+	statement.Exec()
+}
+
+func cleanSongs() {
+	statement, err := database.Connection.Prepare("DELETE FROM songs")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer statement.Close()
+	statement.Exec()
+}
+
+func TestMain(m *testing.M) {
+	c := setup()
+
+	os.Exit(m.Run())
+
+	teardown(c)
+}
+func TestAddSongToDatabase(t *testing.T) {
+	cleanSongs()
+
+	name, exists := voice.DB.SongExists("Youtube ID")
+	assert.False(t, exists)
+
+	err := voice.DB.AddSongToDB("Song Name", "Youtube ID")
+	assert.Nil(t, err)
+
+	name, exists = voice.DB.SongExists("Youtube ID")
+	assert.True(t, exists)
+	assert.Equal(t, name, "Song Name")
+}
