a81eaa37a7dc016ea1f92a42a9993c3e317f30b6
James Tan <james_tan97@outlook.com>
2020-11-03 23:47:12 +0800

    Migrate from rylio ytdl to kkdai/youtube

--- models/songs.go
+++ models/songs.go
@@ -16,7 +16,7 @@ type Song struct {
 func (db *DB) AddSongToDB(name string, youtubeID string) error {
 	song := Song{Name: name, YoutubeID: youtubeID}
 
-	return db.Create(song).Error
+	return db.Create(&song).Error
 }
 
 // SongExists checks if a given youtubeID corresponds to a song in the models
