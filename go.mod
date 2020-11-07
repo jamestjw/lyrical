7982f9cac3fd683b56250a897cc448d31d9f0587
James Tan Juan Whei <james_tan97@outlook.com>
2020-11-07 19:37:13 +0800

    bugs/fix youtube api integration (#27)
    * Migrate from rylio ytdl to kkdai/youtube
    
    * Download audio files directly if possible

--- go.mod
+++ go.mod
@@ -7,17 +7,16 @@ require (
 	github.com/bwmarrin/discordgo v0.22.0
 	github.com/golang/mock v1.2.0
 	github.com/gorilla/websocket v1.4.2 // indirect
-	github.com/jamestjw/ytdl v0.6.3-0.20200404070723-39d27bc045eb
 	github.com/jinzhu/gorm v1.9.12
 	github.com/jonas747/dca v0.0.0-20190317094138-10e959e9d3e8
 	github.com/jonas747/ogg v0.0.0-20161220051205-b4f6f4cf3757 // indirect
+	github.com/kkdai/youtube/v2 v2.1.1
 	github.com/mattn/go-sqlite3 v2.0.3+incompatible // indirect
 	github.com/olekukonko/tablewriter v0.0.4
 	github.com/rs/zerolog v1.19.0
 	github.com/rylio/ytdl v0.6.3
 	github.com/sirupsen/logrus v1.5.0
-	github.com/stretchr/testify v1.4.0
-	github.com/urfave/cli v1.22.4 // indirect
+	github.com/stretchr/testify v1.6.1
 	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
 	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
 	golang.org/x/sys v0.0.0-20200929083018-4d22bbb62b3c // indirect
--- ytmp3/ytmp3.go
+++ ytmp3/ytmp3.go
@@ -7,40 +7,34 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 
 	"github.com/jamestjw/lyrical/utils"
-	zerolog "github.com/rs/zerolog/log"
-	"github.com/rylio/ytdl"
+	ytmeta "github.com/kkdai/youtube/v2"
+	ytdl "github.com/kkdai/youtube/v2/downloader"
 )
 
 // AudioPath is the path that contains all audio files
 const AudioPath = "audio-cache"
 
 func init() {
-	_ = os.Mkdir(AudioPath, 0777)
+	os.Mkdir(AudioPath, 0777)
 }
 
 // Download a MP3 file based on youtube ID
 func Download(youtubeID string) (title string, err error) {
-	ctx := context.Background()
-	client := ytdl.Client{
+	client := ytmeta.Client{
 		HTTPClient: http.DefaultClient,
-		Logger:     zerolog.Logger,
 	}
 
 	utils.LogInfo("download", utils.KvForEvent("ytmp3", utils.KVs("youtubeID", youtubeID)))
 
-	vid, err := client.GetVideoInfo(ctx, "https://www.youtube.com/watch?v="+youtubeID)
+	vid, err := client.GetVideo("https://www.youtube.com/watch?v=" + youtubeID)
 	if err != nil {
 		log.Error("Failed to get video info: " + youtubeID)
 		return "", errors.New("video ID is invalid")
-	}
-	ffmpeg, err := exec.LookPath("ffmpeg")
-	if err != nil {
-		log.Println("ffmpeg not found")
-		return "", err
 	}
 
 	title = vid.Title
@@ -48,28 +42,109 @@ func Download(youtubeID string) (title string, err error) {
 		return "", err
 	}
 
-	videoFname := filepath.Join(AudioPath, youtubeID+".mp4")
-	mp3Fname := filepath.Join(AudioPath, youtubeID+".mp3")
-	file, _ := os.Create(videoFname)
-	defer file.Close()
-	defer os.Remove(videoFname)
+	format, audioFormatFound := findAudioFormat(vid.Formats)
 
-	client.Download(ctx, vid, vid.Formats[0], file)
-
-	utils.LogInfo("video_ready", utils.KVs("name", title, "youtubeID", youtubeID, "event", "ytmp3"))
-
-	cmd := exec.Command(ffmpeg, "-y", "-loglevel", "quiet", "-i", videoFname, "-vn", mp3Fname)
-	if err := cmd.Run(); err != nil {
-		utils.LogError("failed audio_extraction", utils.KVs("err", err.Error(), "youtubeID", youtubeID, "event", "ytmp3"))
-		return "", err
+	if audioFormatFound {
+		err = handleAudioFormat(client, vid, format)
+	} else {
+		err = handleVideoFormat(client, vid, format)
 	}
 
-	utils.LogInfo("audio_extracted", utils.KVs("filename", mp3Fname, "youtubeID", youtubeID, "event", "ytmp3"))
+	if err != nil {
+		return "", err
+	}
 
 	return title, nil
 }
 
 // PathToAudio returns a path to an audio file
 func PathToAudio(youtubeID string) string {
-	return filepath.Join(AudioPath, youtubeID+".mp3")
+	return filepath.Join(AudioPath, youtubeID)
+}
+
+func findAudioFormat(formats ytmeta.FormatList) (*ytmeta.Format, bool) {
+	if format, found := filterFormatListByMime(formats, "audio/mp4"); found {
+
+		utils.LogInfo("audio_format_found", utils.KVs("url", format.URL, "event", "ytmp3"))
+		return format, true
+
+	} else if format, found := filterFormatListByMime(formats, "mp4"); found {
+		utils.LogInfo("mp4_format_found", utils.KVs("url", format.URL, "event", "ytmp3"))
+		return format, false
+	}
+
+	defaultFormat := formats[0]
+	utils.LogError("default_video_format", utils.KVs("url", defaultFormat.URL, "event", "ytmp3"))
+	return &defaultFormat, false
+}
+
+func filterFormatListByMime(formats ytmeta.FormatList, query string) (*ytmeta.Format, bool) {
+	for _, format := range formats {
+		if strings.ContainsAny(format.MimeType, query) {
+			return &format, true
+		}
+	}
+	return nil, false
+}
+
+func handleVideoFormat(client ytmeta.Client, vid *ytmeta.Video, format *ytmeta.Format) error {
+	ffmpeg, err := exec.LookPath("ffmpeg")
+	if err != nil {
+		log.Println("ffmpeg not found")
+		return err
+	}
+
+	youtubeID := vid.ID
+	videoFname := filepath.Join(AudioPath, youtubeID+".mp4")
+	mp3Fname := PathToAudio(youtubeID)
+
+	// Cleanup resulting video file downloaded by downloadByFormat
+	defer os.Remove(videoFname)
+
+	err = downloadByFormat(client, vid, format, videoFname)
+	if err != nil {
+		return err
+	}
+	utils.LogInfo("video_ready", utils.KVs("name", vid.Title, "youtubeID", youtubeID, "event", "ytmp3"))
+
+	cmd := exec.Command(ffmpeg, "-y", "-loglevel", "quiet", "-i", videoFname, "-vn", mp3Fname)
+	if err := cmd.Run(); err != nil {
+		utils.LogError("failed audio_extraction", utils.KVs("err", err.Error(), "youtubeID", youtubeID, "event", "ytmp3"))
+		return err
+	}
+
+	utils.LogInfo("audio_extracted", utils.KVs("filename", mp3Fname, "youtubeID", youtubeID, "event", "ytmp3"))
+
+	return nil
+}
+
+func handleAudioFormat(client ytmeta.Client, vid *ytmeta.Video, format *ytmeta.Format) error {
+
+	mp3Fname := PathToAudio(vid.ID)
+
+	err := downloadByFormat(client, vid, format, mp3Fname)
+
+	if err != nil {
+		return err
+	}
+
+	utils.LogInfo("audio_downloaded", utils.KVs("filename", mp3Fname, "youtubeID", vid.ID, "event", "ytmp3"))
+	return nil
+}
+
+func downloadByFormat(client ytmeta.Client, vid *ytmeta.Video, format *ytmeta.Format, fname string) error {
+
+	ctx := context.Background()
+
+	downloader := ytdl.Downloader{
+		client,
+		"",
+	}
+	err := downloader.Download(ctx, vid, format, fname)
+	if err != nil {
+		utils.LogError("video_download_failed", utils.KVs("name", vid.Title, "youtubeID", vid.ID, "event", "ytmp3", "err", err.Error()))
+		return err
+	}
+
+	return nil
 }
